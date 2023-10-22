6afe5a58651d104728b712d56a6963f139d67d54
Fiivezin <138714392+Fiivezin@users.noreply.github.com>
2023-10-22 02:48:10 -0300

    Delete Lucasmilk directory

--- Lucasmilk/manutencao/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const voltarPaginaButton = document.getElementById("voltarPagina");
-
-    voltarPaginaButton.addEventListener("click", function() {
-        window.history.back();
-    });
-});
